ec21b733abb823c42d72f0d5b1bcbcd1c4212789
Matan Slasky <matanslasky@gmail.com>
2026-02-03 22:12:32 +0200

    chore: Add Sentry configuration notes for future error tracking setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './contexts/ThemeContext'
 
+// Sentry error tracking (configured via @sentry/react in App.jsx ErrorBoundary)
+// To enable: Add VITE_SENTRY_DSN to .env file
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
